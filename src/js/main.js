6d47da2d54048509292251880b7b72c42a8fea34
María Sánchez <msanchezroman95@gmail.com>
2023-06-10 22:10:14 +0200

    add fav characters  event

--- src/js/main.js
+++ src/js/main.js
@@ -5,24 +5,51 @@ const ulElement = document.querySelector('.js_char_list');
 const URL = 'https://api.disneyapi.dev/character?pageSize=50';
 
 let charactersList = [];
-function renderCharacters() {
-  ulElement.innerHTML = '';
-  for (const eachCharacter of charactersList) {
-    const characterImg = eachCharacter.imageUrl;
-    const characterName = eachCharacter.name;
-    ulElement.innerHTML += `
-      <li class="character__item">
-        <img class="character__item-img" src="${characterImg}" alt="Disney character">
-        <p class="character__item-name">${characterName}</p>
-      </li>`;
+
+function renderOneCharacter(character) {
+  let html = `
+    <li class="character__item js_character_item" id="${character._id}">
+      <img class="character__item-img" src="${character.imageUrl}" alt="Disney character">
+      <p class="character__item-name">${character.name}</p>
+    </li>`;
+  if (character.imageUrl === '') {
+    const defaultImg = 'https://fakeimg.pl/210x295?text=Disney&font=bebas';
+    html = `
+    <li class="character__item js_character_item" id="${character._id}">
+      <img class="character__item-img" src="${defaultImg}" alt="Disney character">
+      <p class="character__item-name">${character.name}</p>
+    </li>`;
   }
+  return html;
+}
+
+function renderAllCharacters() {
+  for (const eachCharacter of charactersList) {
+    ulElement.innerHTML += renderOneCharacter(eachCharacter);
+  }
+  addEventCharacter();
 }
 
 // Favoritos
+let favCharacters = [];
 
-const handleClickFavourite(ev) {
-    
+function addEventCharacter() {
+  const liElementList = document.querySelectorAll('.js_character_item');
+  for (const eachLi of liElementList) {
+    eachLi.addEventListener('click', handleClickFav);
+  }
 }
+
+const handleClickFav = (ev) => {
+  const id = parseInt(ev.currentTarget.id);
+  const selectedCharacter = charactersList.find((item) => item._id === id);
+  const indexCharacter = favCharacters.findIndex((item) => item._id === id);
+
+  if (indexCharacter === -1) {
+    favCharacters.push(selectedCharacter);
+  }
+  console.log(favCharacters);
+};
 
 // fetch y eventos
 
@@ -30,10 +57,8 @@ fetch(URL)
   .then((response) => response.json())
   .then((content) => {
     charactersList = content.data;
-    renderCharacters();
+    renderAllCharacters();
   })
   .catch((error) => {
     console.log(error);
   });
-
-ulElement.addEventListener('click', handleClickFavourite);
