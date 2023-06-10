4310641d7d2a6baae145e50fdf11a1b325602276
María Sánchez <msanchezroman95@gmail.com>
2023-06-10 20:41:18 +0200

    add connection to server and render function

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-console.log('>> Ready :)');
+const ulElement = document.querySelector('.js_char_list');
+
+const URL = 'https://api.disneyapi.dev/character?pageSize=50';
+
+let charactersList = [];
+function renderCharacters() {
+  ulElement.innerHTML = '';
+  for (const eachCharacter of charactersList) {
+    const characterImg = eachCharacter.imageUrl;
+    const characterName = eachCharacter.name;
+    ulElement.innerHTML += `
+      <li class="character__item">
+        <img class="character__item-img" src="${characterImg}" alt="Disney character">
+        <p class="character__item-name">${characterName}</p>
+      </li>`;
+  }
+}
+
+// Favoritos
+
+const handleClickFavourite(ev) {
+    
+}
+
+// fetch y eventos
+
+fetch(URL)
+  .then((response) => response.json())
+  .then((content) => {
+    charactersList = content.data;
+    renderCharacters();
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+ulElement.addEventListener('click', handleClickFavourite);
