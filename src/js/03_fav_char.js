850e114bb61d33dbc1801592cb978a02be342c07
María Sánchez <msanchezroman95@gmail.com>
2023-06-13 09:58:19 +0200

    change favorites function

--- src/js/03_fav_char.js
+++ src/js/03_fav_char.js
@@ -3,19 +3,11 @@
 // event click function
 const handleClickFav = (ev) => {
   const id = parseInt(ev.currentTarget.id);
-  // Array validation necessary to add favs in search
   // const index declared outside of condition to use it twice
   const indexCharacter = favCharacters.findIndex((item) => item._id === id);
-  if (Array.isArray(charactersList)) {
-    const selectedCharacter = charactersList.find((item) => item._id === id);
-    if (indexCharacter === -1) {
-      favCharacters.push(selectedCharacter);
-    }
-  } else if (Array.isArray(charactersList) === false) {
-    //when charactersList is not array push the object directly
-    if (indexCharacter === -1) {
-      favCharacters.push(charactersList);
-    }
+  const selectedCharacter = charactersList.find((item) => item._id === id);
+  if (indexCharacter === -1) {
+    favCharacters.push(selectedCharacter);
   }
   renderFavCharacters();
 };
