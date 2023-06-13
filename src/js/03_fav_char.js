65b4e84ed5f336044bafbea998a21a802eafaf0b
María Sánchez <msanchezroman95@gmail.com>
2023-06-13 13:13:03 +0200

    delete comment in fav file

--- src/js/03_fav_char.js
+++ src/js/03_fav_char.js
@@ -3,7 +3,6 @@
 // event click function to mark favs
 const handleClickFav = (ev) => {
   const id = parseInt(ev.currentTarget.id);
-  // const index declared outside of condition to use it twice
   const indexCharacter = favCharacters.findIndex((item) => item._id === id);
   const selectedCharacter = charactersList.find((item) => item._id === id);
   if (indexCharacter === -1) {
